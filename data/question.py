a120ebf8ca4c3d8a34eca3e56e5ba5222eec31dd
esyolal@gmail.com <esyolal@gmail.com>
2025-07-03 13:36:58 +0300

    Add answers

--- data/question.py
+++ data/question.py
@@ -17,7 +17,7 @@ def connect_db():
 def question_1_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT * FROM students WHERE age > 22;')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -27,7 +27,7 @@ def question_1_query():
 def question_2_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute("SELECT * FROM courses WHERE category = 'Veritabanı';")
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -37,7 +37,7 @@ def question_2_query():
 def question_3_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute("SELECT * FROM students WHERE first_name LIKE 'A%';")
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -47,7 +47,7 @@ def question_3_query():
 def question_4_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute("SELECT * FROM courses WHERE course_name LIKE '%SQL%';")
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -57,7 +57,7 @@ def question_4_query():
 def question_5_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute("SELECT * FROM students WHERE age BETWEEN 22 AND 24;")
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -67,7 +67,7 @@ def question_5_query():
 def question_6_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT s.first_name, s.last_name FROM students AS s JOIN enrollments e ON s.student_id = e.student_id;')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -77,7 +77,7 @@ def question_6_query():
 def question_7_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute("SELECT c.course_name, COUNT(e.enrollment_id) AS student_count FROM courses c JOIN enrollments e ON c.course_id = e.course_id WHERE c.category = 'Veritabanı' GROUP BY c.course_name;")
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -87,7 +87,7 @@ def question_7_query():
 def question_8_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT c.course_name, i.name AS instructor_name FROM courses c JOIN course_instructors ci ON c.course_id = ci.course_id JOIN instructors i ON ci.instructor_id = i.instructor_id;')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -97,7 +97,7 @@ def question_8_query():
 def question_9_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT * FROM students WHERE student_id NOT IN (SELECT student_id FROM enrollments);')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -107,7 +107,7 @@ def question_9_query():
 def question_10_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT c.course_name, AVG(s.age) AS avg_age FROM courses c JOIN enrollments e ON c.course_id = e.course_id JOIN students s ON s.student_id = e.student_id GROUP BY c.course_name;')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -117,7 +117,7 @@ def question_10_query():
 def question_11_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT s.first_name, s.last_name, COUNT(e.course_id) AS total_courses FROM students s JOIN enrollments e ON s.student_id = e.student_id GROUP BY s.student_id, s.first_name, s.last_name;')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -127,7 +127,7 @@ def question_11_query():
 def question_12_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT i.name AS instructor_name, COUNT(ci.course_id) AS total_courses FROM instructors i JOIN course_instructors ci ON i.instructor_id = ci.instructor_id GROUP BY i.instructor_id, i.name HAVING COUNT(ci.course_id) > 1;')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -137,7 +137,7 @@ def question_12_query():
 def question_13_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT c.course_name, COUNT(DISTINCT e.student_id) AS unique_students FROM courses c JOIN enrollments e ON c.course_id = e.course_id GROUP BY c.course_name;')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -147,7 +147,7 @@ def question_13_query():
 def question_14_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute("SELECT s.first_name, s.last_name FROM students s WHERE s.student_id IN (SELECT e1.student_id FROM enrollments e1 JOIN courses c1 ON e1.course_id = c1.course_id WHERE c1.course_name = 'SQL Temelleri') AND s.student_id IN (SELECT e2.student_id FROM enrollments e2 JOIN courses c2 ON e2.course_id = c2.course_id WHERE c2.course_name = 'İleri SQL');")
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -157,7 +157,7 @@ def question_14_query():
 def question_15_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT s.first_name, s.last_name, c.course_name, i.name AS instructor_name, e.enrollment_date FROM enrollments e JOIN students s ON e.student_id = s.student_id JOIN courses c ON e.course_id = c.course_id JOIN course_instructors ci ON c.course_id = ci.course_id JOIN instructors i ON ci.instructor_id = i.instructor_id;')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
